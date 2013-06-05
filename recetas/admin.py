3d04f449b462db30a3616eb4715d355ff535a321
David Lopez Recio <dalore1es@gmail.com>
2013-06-05 16:55:42 +0200

    Add new model Bebida

---
+++ recetas/admin.py
@@ -0,0 +1,5 @@
+from recetas.models import Bebida
+from django.contrib import admin
+
+
+admin.site.register(Bebida)
--- recetas/views.py
+++ recetas/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from recetas.models import Bebida
+from django.shortcuts import render_to_response
+
+def lista_bebidas(request):
+    bebidas = Bebida.objects.all()
+    return render_to_response('lista_bebidas.html', {'bebidas':bebidas})
