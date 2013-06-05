ac1209d5802dc2f3bffeaa52cab622f60971438c
David Lopez Recio <dalore1es@gmail.com>
2013-06-05 17:02:58 +0200

    Correct models.py Bebida

--- recetas/models.py
+++ recetas/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Bebida(models.Model):
     nombre = models.CharField(max_length = 50)
-    ingredientes = model.TextField()
-    preparacion = model.TextField()
+    ingredientes = models.TextField()
+    preparacion = models.TextField()
 
     def __unicode__(self):
         return self.nombre
