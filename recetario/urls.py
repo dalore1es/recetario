1b973eead382180ed4851d94de26857551fc0613
David Lopez Recio <dalore1es@gmail.com>
2013-06-13 12:52:32 +0200

    Remove model Bebida

--- recetario/urls.py
+++ recetario/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'recetas.views.lista_bebidas'),
+    url(r'^about/$', 'recetas.views.about'),
 )
--- recetas/admin.py
+++ recetas/admin.py
@@ -1,6 +1,6 @@
-from recetas.models import Bebida, Receta
+from recetas.models import Receta, Comentario
 from django.contrib import admin
 
 
-admin.site.register(Bebida)
 admin.site.register(Receta)
+admin.site.register(Comentario)
